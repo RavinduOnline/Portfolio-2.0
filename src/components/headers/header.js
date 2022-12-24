9c0198429fdad548a8e4031fac9bec81bdcc351e
RavinduOnline <ravindurasanga99@gmail.com>
2022-12-24 21:52:55 +0530

    animated header added

--- src/components/headers/header.js
+++ src/components/headers/header.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import {React,  useState} from 'react'
 
 const navItems = [
     {
@@ -29,10 +29,30 @@ const navItems = [
 
 export default function Header() {
 
+  const [navbar, setNavbar] = useState(false);
+  const [navbarCSS, setNavbarCSS] = useState("");
+  const [navbarItemCSS, setNavbarItemCSS] = useState("text-black");
+
+
+
+  const changeBackground = () => {
+    if(window.scrollY >= 7 ){
+        setNavbar(true);
+        setNavbarCSS("bg-black");
+        setNavbarItemCSS("text-white");
+    }
+    else{
+      setNavbar(false);
+      setNavbarCSS("");
+      setNavbarItemCSS("text-black")
+    }
+  }
+
+  window.addEventListener('scroll', changeBackground);
 
   return (
-    <div className='w-full  fixed  bg-black top-0' style={{ zIndex: 999}}>
-        <nav className='mx-auto px-52  flex pb-4 justify-between border-b-4 border-gray-200'>
+    <div className={`w-full fixed top-0 ${navbarCSS}`} style={{ zIndex: 999}}>
+        <nav className='mx-auto px-52  flex pb-4 justify-between '>
             <div className='flex w-full'>
                 <div className=' mr-16 bg-black max-w-fit  pt-6 pb-4 px-6 rounded-br-2xl rounded-bl-2xl'>
                     <img className='h-12' src="./images/ravindu-logo.png" alt="" />
@@ -42,7 +62,7 @@ export default function Header() {
                     {navItems.map(item=>{
 
                       return(
-                        <a href={item.link} className=' text-white font-bold pt-6 pb-4 px-6 rounded-br-2xl rounded-bl-2xl flex items-end hover:bg-red-500 hover:text-white ' onMouseOver="this.style.color='#0F0'"
+                        <a href={item.link} className={` ${navbarItemCSS} font-bold pt-6 pb-4 px-6 rounded-br-2xl rounded-bl-2xl flex items-end hover:bg-red-500 hover:text-white`} onMouseOver="this.style.color='#0F0'"
                         >
                             {item.text}
                         </a>
